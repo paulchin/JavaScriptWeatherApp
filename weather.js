80c21965668babd48d9d8b6c088e8b979db5dcd5
Mark Lassoff <marklassoff@Marks-MBP.home>
2019-04-09 11:23:15 -0400

    JavaScript in progress

--- weather.js
+++ weather.js
@@ -2,7 +2,27 @@ const btnWeather = document.getElementById("btnWeather");
 const txtCity = document.getElementById("txtCity");
 
 btnWeather.onclick = function(){
-  let city = txtCity.value;
-  let URL = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${KEY}`;
-  
+  const city = txtCity.value;
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${KEY}`;
+  fetch(url).then(response => {
+  response.json().then(json => {
+    let data = json;
+    formatResponse(data);
+  });
+});
+}
+
+function kelvinToFahrenheit(kTemp){
+  const fTemp = kTemp * (9/5) - 459.67;
+  return fTemp;
+}
+
+function formatResponse(data){
+  console.log(data);
+  let out = `<h1>Current Condtions for ${data.name}</h1>
+  <p><strong>Temperature:</strong> ${Math.round(kelvinToFahrenheit(data.main.temp))}`
+
+
+
+  console.log(out);
 }
